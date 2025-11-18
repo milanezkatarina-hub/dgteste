af2779bb6a8aa926d0e03f9d4f5c8c628ac41097
milanezkatarina <milanezkatarina@gmail.com>
2025-11-18 00:01:05 -0300

    Agora temos templates!

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render, 
+from django.shortcuts import render,  get_object_or_404, redirect
 from django.http import HttpResponse
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+   return render(request, 'blog/post_list.html', {})
+
+def portao(request):
+    return HttpResponse('Você chegou ao portão da casa!')
+
+def sala(request):
+    return HttpResponse("Você chegou na sala. Senta no sofá!")
+
+def quarto(request):
+    return HttpResponse("Agora está no quarto, pode se deitar!")
